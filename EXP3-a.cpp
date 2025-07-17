fc26325c142c324a560419f4e111854fe24b17e1
Harshh00100 <kunarharsh76@gmail.com>
2025-07-17 19:08:15 +0530

    EXP3-a.cpp

---
+++ EXP3-a.cpp
@@ -0,0 +1,21 @@
+//Harsh kumar
+//prn 24070123146
+//B3
+#include <iostream>
+using namespace std ;
+int main(){
+    cout << "input the number " ; //input
+    int n;
+    cin >> n ; //input assigning to a variable
+    if (n<0){  //if condition
+        cout << "number is negative" ; //output if condition true
+    }
+    else if (n>0)
+    {
+        cout << "number is positive" ;
+    }
+    else{
+    cout << "number is zero" ;}
+    
+return 0;
+}
